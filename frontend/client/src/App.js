27bc5c2df96c85c2d7c4e211caeb9a45f3a8541e
shivanshsharma10 <outcastedstory@gmail.com>
2025-11-11 02:38:15 +0530

    api changed

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import axios from 'axios';
 
-const API_URL = 'https://my-note-api-chd3.onrender.com'
+const API_URL = 'https://my-note-api-chd3.onrender.com/api/notes'
 
 function App(){
 const [notes, setNotes] = useState([]);
